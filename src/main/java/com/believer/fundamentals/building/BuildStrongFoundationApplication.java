1eef48b455ee717477aa42f1d1eb793f799fe780
karthikt89 <121148099+karthikt89@users.noreply.github.com>
2023-04-11 13:25:41 +0530

    Initial version

---
+++ src/main/java/com/believer/fundamentals/building/BuildStrongFoundationApplication.java
@@ -0,0 +1,13 @@
+package com.believer.fundamentals.building;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildStrongFoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildStrongFoundationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/believer/fundamentals/building/BuildStrongFoundationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.believer.fundamentals.building;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildStrongFoundationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
